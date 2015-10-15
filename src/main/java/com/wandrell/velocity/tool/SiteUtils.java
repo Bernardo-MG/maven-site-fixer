12988f1d178b3778546b6f4574dfc65078362b24
Bernardo Martínez Garrido <programming@wandrell.com>
2015-10-15 23:10:17 +0200

    Added a method to fix local anchors.

--- src/main/java/com/wandrell/velocity/tool/SiteUtils.java
+++ src/main/java/com/wandrell/velocity/tool/SiteUtils.java
@@ -73,6 +73,34 @@ public class SiteUtils {
     }
 
     /**
+     * Returns the result from fixing links to anchors in the same page on the
+     * received HTML code.
+     * <p>
+     * The href value will be in lower case, and with spaces and points removed.
+     * 
+     * @param html
+     *            HTML with anchors to fix
+     * @return HTML content, with the anchor href attribute fixed
+     */
+    public final String fixAnchorLinks(final String html) {
+        final Element body;     // Body of the HTML code
+        String ref;             // Value of the href attribute
+
+        body = Jsoup.parse(html).body();
+
+        // Anchors
+        for (final Element child : body.getElementsByTag("a")) {
+            ref = child.attr("href");
+
+            if (ref.substring(0, 1).equals("#")) {
+                child.attr("href", ref.toLowerCase().replaceAll("[ _.]", ""));
+            }
+        }
+
+        return body.html();
+    }
+
+    /**
      * Returns the result from adding or fixing ids to the headings on the
      * received HTML code.
      * <p>
@@ -81,7 +109,7 @@ public class SiteUtils {
      * case, with spaces and points removed.
      * 
      * @param html
-     *            HTML with headings where and id should be added
+     *            HTML with headings where an id should be added
      * @return HTML content, with the tables transformed
      */
     public final String fixHeadingIds(final String html) {
@@ -200,20 +228,10 @@ public class SiteUtils {
             element.remove();
         } else if (report.equals("surefire-report")) {
             body.getElementsByTag("h2").iterator().next().tagName("h1");
-
-            for (final Element child : body.getElementsByTag("a")) {
-                child.attr("href", child.attr("href").toLowerCase()
-                        .replaceAll("[ _.]", ""));
-            }
         } else if (report.equals("failsafe-report")) {
             newElement = body.getElementsByTag("h2").iterator().next();
             newElement.tagName("h1");
             newElement.text("Failsafe Report");
-
-            for (final Element child : body.getElementsByTag("a")) {
-                child.attr("href", child.attr("href").toLowerCase()
-                        .replaceAll("[ _.]", ""));
-            }
         } else if (report.equals("checkstyle")) {
             body.getElementsByTag("h2").iterator().next().tagName("h1");
             body.select("img[src=\"images/rss.png\"]").remove();
