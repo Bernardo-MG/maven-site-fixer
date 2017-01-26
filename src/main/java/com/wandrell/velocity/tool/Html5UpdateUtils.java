70e5ea879c91c79169c407092b9fbe47663fbf89
Bernardo <programming@wandrell.com>
2017-01-26 22:56:48 +0100

    Removed methods which were too complex

--- src/main/java/com/wandrell/velocity/tool/Html5UpdateUtils.java
+++ src/main/java/com/wandrell/velocity/tool/Html5UpdateUtils.java
@@ -69,60 +69,6 @@ public class Html5UpdateUtils {
     }
 
     /**
-     * Returns the result from fixing internal links which are using point
-     * separators. It also takes care of removing the points from the internal
-     * ids. Using points for internal anchors can break navigation.
-     * <p>
-     * The fix consists on just removing all points from all the ids. For
-     * example "id.with.points" will become "idwithpoints".
-     * 
-     * @param html
-     *            HTML to fix internal links
-     * @return HTML content, with the points removed from internal links and ids
-     */
-    public final String fixInternalLinks(final String html) {
-        final Element body; // Body of the HTML code
-
-        checkNotNull(html, "Received a null pointer as html");
-
-        body = Jsoup.parse(html).body();
-
-        // Removes points from the id attributes
-        removePointsFromAttr(body, "[id]", "id");
-        // Removes points from the href attributes for internal links
-        removePointsFromAttr(body, "[href^=\"#\"]", "href");
-
-        return body.html();
-    }
-
-    /**
-     * Returns the result from removing the {@code externalLink} class from
-     * links from the received HTML code.
-     * <p>
-     * These are used by Doxia but are meaningless for most modern UI
-     * frameworks, such as Bootstrap.
-     * <p>
-     * If a after removing the class any link ends without classes, then the
-     * {@code class} attribute will be removed too.
-     * 
-     * @param html
-     *            HTML where the {@code externalLink} class is to be removed
-     * @return HTML content with the {@code externalLink} class removed
-     */
-    public final String removeExternalLinks(final String html) {
-        final Element body;            // Body of the HTML code
-
-        checkNotNull(html, "Received a null pointer as html");
-
-        body = Jsoup.parse(html).body();
-
-        // <a> elements with the externalLink class
-        getHtmlUtils().removeClass(body, "a.externalLink", "externalLink");
-
-        return body.html();
-    }
-
-    /**
      * Returns the result from removing links with no {@code href} attribute
      * defined from the received HTML code.
      * <p>
@@ -204,66 +150,6 @@ public class Html5UpdateUtils {
     }
 
     /**
-     * Returns the result from updating and correcting source divisions on the
-     * received HTML code.
-     * <p>
-     * Outdated source divisions such as {@code 
-     * <div class="source">} are transformed to the new {@code <code>} elements.
-     * Additionally, it will correct the position of the {@code pre} element,
-     * moving it out of the code section.
-     * <p>
-     * It also fixes a Doxia error where the source division is wrapped by a
-     * second source division.
-     * 
-     * @param html
-     *            HTML where the source sections are to be updated
-     * @return HTML content, with the source sections updated
-     */
-    public final String updateCodeSections(final String html) {
-        final Element body; // Body of the HTML code
-
-        checkNotNull(html, "Received a null pointer as html");
-
-        body = Jsoup.parse(html).body();
-
-        // Source divs are transformed to code tags
-        getHtmlUtils().retag(body, "div.source", "code");
-        // Removes redundant tags
-        getHtmlUtils().unwrap(body, "code > code");
-
-        getHtmlUtils().swapTagWithParent(body, "code > pre");
-
-        // Removes source class from code tags
-        getHtmlUtils().removeClass(body, "code.source", "source");
-
-        return body.html();
-    }
-
-    /**
-     * Returns the result from updating section divisions, such as {@code 
-     * <div class="section">}, to the new {@code <section>} element on the
-     * received HTML code.
-     * 
-     * @param html
-     *            HTML where the section divisions are to be updated
-     * @return HTML content, with the section divisions updated
-     */
-    public final String updateSectionDiv(final String html) {
-        final Element body;                  // Body of the HTML code
-
-        checkNotNull(html, "Received a null pointer as html");
-
-        body = Jsoup.parse(html).body();
-
-        // Section divs are transformed to section tags
-        getHtmlUtils().retag(body, "div.section", "section");
-        // Removes section class from section tags
-        getHtmlUtils().removeClass(body, "section.section", "section");
-
-        return body.html();
-    }
-
-    /**
      * Corrects table headers by adding a {@code <thead>} section where missing.
      * <p>
      * This serves to fix an error with tables created by Doxia, which will add
@@ -317,39 +203,6 @@ public class Html5UpdateUtils {
         body = Jsoup.parse(html).body();
 
         updateTableHeads(body);
-
-        return body.html();
-    }
-
-    /**
-     * Returns the result from updating the tables on the received HTML code.
-     * <p>
-     * This method will add the missing {@code <thead>} element to table, remove
-     * the unneeded border attribute and remove the {@code bodyTable} class.
-     * <p>
-     * It also removes the alternating rows attributes. Doxia marks them with
-     * the {@code a} and {@code b} classes. This seems to be an outdated method
-     * to get alternating colored rows.
-     * 
-     * @param html
-     *            HTML with tables to update
-     * @return HTML content, with the tables updated
-     */
-    public final String updateTables(final String html) {
-        final Element body; // Body of the HTML code
-
-        checkNotNull(html, "Received a null pointer as html");
-
-        body = Jsoup.parse(html).body();
-
-        // Removes bodyTable from tables
-        getHtmlUtils().removeClass(body, "table.bodyTable", "bodyTable");
-        updateTableHeads(body);
-        // Removes border attribute
-        getHtmlUtils().removeAttribute(body, "table[border]", "border");
-        // Removes alternating rows classes
-        getHtmlUtils().removeClass(body, "tr.a", "a");
-        getHtmlUtils().removeClass(body, "tr.b", "b");
 
         return body.html();
     }
